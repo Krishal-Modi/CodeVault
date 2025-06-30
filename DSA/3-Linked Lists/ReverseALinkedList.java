a2a0af5dc92705f5a7e95147564d8c13309e0c2f
Krishal <krishalrmodi@gmail.com>
2025-06-30 19:58:43 +0530

    Added Code

--- DSA/3-Linked Lists/ReverseALinkedList.java
+++ DSA/3-Linked Lists/ReverseALinkedList.java
@@ -23,18 +23,6 @@
         }
     }
 
-    
-    // Add at first
-    public void addFirst(String data) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = newNode;
-            return;
-        }
-        newNode.next = head;
-        head = newNode;
-    }
-
     // Add at last
     public void addLast(String data) {
         Node newNode = new Node(data);
@@ -48,38 +36,6 @@
         }
         current.next = newNode;
     }
-
-    // Delete First
-    public void deleteFirst(){
-        if(head == null) {
-            System.out.println("List is empty, nothing to delete.");
-            return;
-        }
-        size--;
-        head = head.next;
-
-   }
-
-    // Delete Last
-    public void deleteLast(){
-        if(head == null) {
-            System.out.println("List is empty, nothing to delete.");
-            return;
-        }
-        size--;
-        if(head.next == null){
-            head = null;
-            return;
-        }
-
-        Node secondLast = head;
-        Node lastNode = head.next;
-        while(lastNode.next != null){
-            lastNode = lastNode.next;
-            secondLast = secondLast.next;
-        }
-        secondLast.next = null;
-   }
 
     // Returning Size of the list
    public int getSize(){
