cb260edad3978ff42c6de9dd32320e80fda4650c
Krishal-Modi <krishalrmodi@gmail.com>
2025-08-07 15:17:48 +0530

    Python

---
+++ Python/16-OOp.py
@@ -0,0 +1,66 @@
+# Structure code using classes objects
+class MyClass:
+    x = 5
+
+p1 = MyClass
+print(p1.x)
+
+# __init__ Method
+# inbuilt method always runs when classes are initiated
+#  The __init__() method is called automatically every time the class is being used to create a new object.
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+
+# __str__ method
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def __str__(self):
+    return f"{self.name}({self.age})"
+
+p1 = Person("John", 36)
+
+print(p1)
+
+
+
+# Example
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def __str__(self):
+    return f"{self.name}({self.age})"
+
+p1 = Person("John", 36)
+
+print(p1)
+
+
+# Delete Object Properties
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+
+del p1.age
+
+# print(p1.age)
